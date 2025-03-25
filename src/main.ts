22cf22214d9a0e402c5286e8f613b56fd23d094c
jangsen <dh7895210@naver.com>
2025-03-25 23:22:38 +0900

    Chore: swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Redis Demo API')
+    .setDescription('레디스 캐싱 및 연습용 프로젝트 API 입니다.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
 
   await app.listen(8000);
 }
