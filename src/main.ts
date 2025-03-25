200b30eebd0b7917d7b9bc0aac0aba6dad74527b
jangsen <dh7895210@naver.com>
2025-03-25 23:18:41 +0900

    Chore: validation 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(8000);
 }
 
